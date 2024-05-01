d5e314cccb86532524e9657fb28edb645939e11d
Sunshine <167001185+0xSunshine@users.noreply.github.com>
2024-05-01 19:18:45 +0300

    discord

--- index.ts
+++ index.ts
@@ -12,7 +12,16 @@ await syncTokens(client, prisma);
 await syncSwaps(client, prisma);
 */
 
+
 import express from 'express';
+import { Client, GatewayIntentBits, Collection, PermissionFlagsBits,} from "discord.js";
+import { config } from "dotenv";
+import { readdirSync } from "fs";
+import { join } from "path";
+
+//import { Command, SlashCommand } from "./types";
+
+config()
 
 const app = express();
 const port = 80;
@@ -24,3 +33,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
+
+const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits
+const client = new Client({intents:[Guilds, MessageContent, GuildMessages, GuildMembers]})
+
+//client.slashCommands = new Collection<string, SlashCommand>()
+//client.commands = new Collection<string, Command>()
+//client.cooldowns = new Collection<string, number>()
+
+//const handlersDir = join(__dirname, "./handlers")
+//readdirSync(handlersDir).forEach(handler => {
+//    if (!handler.endsWith(".js")) return;
+//    require(`${handlersDir}/${handler}`)(client)
+//})
+
+client.login(process.env.DISCORD_BOT_TOKEN).then()
